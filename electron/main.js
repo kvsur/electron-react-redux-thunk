70946b3281c3a386494d5f207644a3717e9976ec
kvsur <hello1024lc@gmail.com>
2019-07-26 10:00:06 +0800

    新增自动更新electron-updater功能

--- electron/main.js
+++ electron/main.js
@@ -1,18 +1,71 @@
 const { app, BrowserWindow, Tray, nativeImage, Menu } = require('electron');
+const { autoUpdater } = require('electron-updater');
 const { ipcMain } = require('electron');
 const path = require('path');
 const appConfig = require('./app.config');
 const Schedule = require('./schedule');
+
+const uploadUrl = '';
 
 // 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
 // 垃圾回收的时候，window对象将会自动的关闭
 let win;
 let tray;
 
+function updateHandle() {
+    let message = {
+        error: '检查更新出错',
+        checking: '正在检查更新……',
+        updateAva: '检测到新版本，正在下载……',
+        updateNotAva: '现在使用的就是最新版本，不用更新',
+    };
+    // const os = require('os');
+
+    autoUpdater.setFeedURL(uploadUrl);
+    autoUpdater.on('error', function (error) {
+        sendUpdateMessage(message.error)
+    });
+    autoUpdater.on('checking-for-update', function () {
+        sendUpdateMessage(message.checking)
+    });
+    autoUpdater.on('update-available', function (info) {
+        sendUpdateMessage(message.updateAva)
+    });
+    autoUpdater.on('update-not-available', function (info) {
+        sendUpdateMessage(message.updateNotAva)
+    });
+
+    // 更新下载进度事件
+    autoUpdater.on('download-progress', function (progressObj) {
+        win.webContents.send('downloadProgress', progressObj)
+    })
+    autoUpdater.on('update-downloaded', function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
+
+        ipcMain.on('isUpdateNow', (e, arg) => {
+            console.log(arguments);
+            console.log("开始更新");
+            //some code here to handle event
+            autoUpdater.quitAndInstall();
+        });
+
+        win.webContents.send('isUpdateNow')
+    });
+
+    ipcMain.on("checkForUpdate", () => {
+        //执行自动更新检查
+        autoUpdater.checkForUpdates();
+    })
+}
+
+// 通过main进程发送事件给renderer进程，提示更新信息
+function sendUpdateMessage(text) {
+    win.webContents.send('process_event', 'udpate-message', text)
+}
+
 function createWindow() {
     tray = new Tray(path.join(__dirname, '../build_web/favicon.ico'));
     // 创建浏览器窗口。
-    win = new BrowserWindow({...appConfig});
+    win = new BrowserWindow({ ...appConfig });
 
     // win.setMenu(null);
 
@@ -82,13 +135,15 @@ function createWindow() {
             win.show();
             global.isAppHide = false;
             // win.webContents.send('process_event', 'classStart', {});
-            win.webContents.send('process_event', 'tray-click', {a: 12});
+            win.webContents.send('process_event', 'tray-click', { a: 12 });
         }
     });
 
     // setTimeout(() => {
     //     win.show();
     // }, 10000);
+
+    updateHandle();
 }
 
 const gotTheLock = app.requestSingleInstanceLock();
