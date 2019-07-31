a92713d92bc06c1c84553ed10dbb2b0e8db19b9b
znkf <znkf@myhexin.com>
2019-07-31 13:45:02 +0800

    自动更新功能更新

--- src/App.js
+++ src/App.js
@@ -3,63 +3,30 @@ import { Route, } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import Router from './router-dom';
-// import Header from './components/Header';
 import store from './store';
 import Login from './pages/Login';
 import Lesson from './pages/Lesson';
 import Ending from './pages/Ending';
-
 import history from './router-dom/history';
-
-
 import styles from './App.less';
 import Header from './components/Header';
-
-import Bridge from './utils/bridge';
-import { message } from 'antd';
+import Updater from './components/Update';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 class App extends Component {
-  state = {};
-
-  componentDidMount() {
-    Bridge.send('check-update');
-    Bridge.on('update-message', this.showUpdateMessage);
-    Bridge.on('download-progress', this.dealProgress);
-    Bridge.on('update-now', this.updateNow);
-  }
-
-  componentWillUnmount() {
-    Bridge.cancel('update-message', this.showUpdateMessage);
-    Bridge.cancel('download-progress', this.dealProgress);
-    Bridge.cancel('update-now', this.updateNow);
-  }
-
-  updateNow = () => {
-    Bridge.send('update-now');
-  }
-
-  showUpdateMessage = msg => {
-    message.warning(msg);
-  }
-
-  dealProgress = progressObj => {
-    // const { transferred, total, bytesPerSecond, } = progressObj;
-    console.log(...progressObj);
-  }
-
   render() {
     return (
       <Provider store={store}>
         <main className={styles.main}>
           <Router history={history}>
-            { isDevelopment ? null : <Header />}
+            {isDevelopment ? null : <Header />}
             <Route path="/" exact component={Login} />
             <Route path="/login" component={Login} />
             <Route path="/lesson" component={Lesson} />
             <Route path="/ending" component={Ending} />
           </Router>
+          {process.env.NODE_ENV === 'production' ? <Updater /> : null}
         </main>
       </Provider>
     );
---
+++ src/components/Update/index.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import Bridge from '../../utils/bridge';
+import { Modal, Progress } from 'antd';
+
+class Updater extends Component {
+  state = {
+    showModal: false,
+    updateText: '正在下载更新......',
+    percent: 0,
+    isAva: false,
+  };
+
+  componentWillMount() {
+    Bridge.on('update-start', this.checkUpateStart);
+    Bridge.on('update-message', this.showUpdateMessage);
+    Bridge.on('download-progress', this.dealProgress);
+    Bridge.on('update-now', this.updateNow);
+    Bridge.on('update-close', this.updateClose);
+    Bridge.on('update-available', this.updateAvailable);
+  }
+
+  componentDidMount() {
+    Bridge.send('check-update');
+  }
+
+  componentWillUnmount() {
+    Bridge.cancel('update-start', this.checkUpateStart);
+    Bridge.cancel('update-message', this.showUpdateMessage);
+    Bridge.cancel('download-progress', this.dealProgress);
+    Bridge.cancel('update-now', this.updateNow);
+    Bridge.cancel('update-close', this.updateClose);
+    Bridge.cancel('update-available', this.updateAvailable);
+  }
+
+  checkUpateStart = () => {
+    this.setState({
+      showModal: true,
+    });
+  }
+
+  updateAvailable = () => {
+    this.setState({
+      isAva: true,
+    });
+  }
+
+  updateClose = error => {
+    console.log(error);
+    setTimeout(() => {
+      this.setState({
+        showModal: false,
+      });
+    }, 2000);
+  }
+
+  updateNow = () => {
+    Bridge.send('update-now');
+    this.setState({
+      showModal: false,
+    });
+  }
+
+  showUpdateMessage = updateText => {
+    // message.warning(msg);
+    this.setState({
+      updateText,
+    });
+  }
+
+  dealProgress = progressObj => {
+    // const { transferred, total, bytesPerSecond, } = progressObj;
+    console.log(...progressObj);
+    let { percent } = progressObj;
+    percent = Math.floor(percent * 100) / 100;
+
+    this.setState({
+      percent,
+    });
+  }
+
+  render() {
+    const { showModal, updateText, percent, isAva } = this.state;
+    return (
+        <Modal
+        visible={showModal}
+        title={null}
+        width={200}
+        footer={null}
+        closable={false}
+        keyboard={false}
+        maskClosable={false}
+        centered
+        >
+        <span style={{ color: '#fb7b1c' }}>{updateText}</span>
+        {
+            isAva ? 
+            <Progress
+            strokeColor={{
+                '0%': '#f3bbbb',
+                '100%': '#87d068',
+            }}
+            percent={percent}
+            size="small"
+            /> : null
+        }
+        </Modal>
+    );
+  }
+}
+
+export default Updater;
