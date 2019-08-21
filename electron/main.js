bb5b27b4bd1bdd10aea1c97b8e38231c1ce83890
znkf <znkf@myhexin.com>
2019-08-21 15:03:55 +0800

    客户端控制服务端的更新及进程管理

--- electron/main.js
+++ electron/main.js
@@ -1,28 +1,38 @@
-const { ipcMain, app, BrowserWindow, Tray, Menu, shell } = require('electron');
+const { ipcMain, app, BrowserWindow, Tray, Menu } = require('electron');
 const { autoUpdater } = require('electron-updater');
 const path = require('path');
 const appConfig = require('./app.config');
 const Schedule = require('./schedule');
-const updateJavaService = require('./service.update');
+// const updateJavaService = require('./service.update');
+const restartJava = require('./restart.java');
+const { JAVA_SERVER_ROOT_NAME, RESTART_TIME_INTERVAL } = require('./constant');
 
 const feedOption = {
     // "provider": "znkf",
     "url": 'http://172.19.80.224/packages/docker_images/teaching_package/'
 };
 
+const rootPath = path.join(__dirname, `../../../../${JAVA_SERVER_ROOT_NAME}`);
+
+let restartTimer = null;
+
 // 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
 // 垃圾回收的时候，window对象将会自动的关闭
 let win;
 let tray;
 
+function process_emiter(...arg) {
+    win.webContents.send('process_event', arg[0], ...arg.splice(1));
+}
+
 function hanldeUpateFromRender() {
     //执行自动更新检查
-    win.webContents.send('process_event', 'update-start');
+    process_emiter('update-start');
     autoUpdater.checkForUpdates();
 }
 
 function updateHandle() {
-    let message = {
+    const message = {
         error: '检查更新出错',
         checking: '正在检查更新...',
         updateAva: '正在下载更新...',
@@ -34,41 +44,40 @@ function updateHandle() {
     autoUpdater.on('error', function (error) {
         // dialog.showErrorBox('error', error)
         sendUpdateMessage(message.error);
-        win.webContents.send('process_event', 'update-close', error);
+        process_emiter('update-close', error);
     });
     autoUpdater.on('checking-for-update', function () {
         sendUpdateMessage(message.checking);
     });
     autoUpdater.on('update-available', async function (info) {
         sendUpdateMessage(message.updateAva);
-        // 更新java 服务
-        // await updateJavaService(win.webContents, app.getPath('userData'), app.getAppPath(), app.getVersion(), true);
-        win.webContents.send('process_event', 'update-available');
+        process_emiter('update-available');
     });
     autoUpdater.on('update-not-available', function (info) {
         sendUpdateMessage(message.updateNotAva);
-        win.webContents.send('process_event', 'update-close', info);
+        process_emiter('update-close', info);
         // 更新java 服务
-        updateJavaService(win.webContents, app.getPath('userData'), app.getAppPath(), app.getVersion(), false);
+        // updateJavaService(win.webContents, app.getPath('userData'), app.getAppPath(), app.getVersion(), false);
     });
 
     // 更新下载进度事件
     autoUpdater.on('download-progress', function (progressObj) {
-        win.webContents.send('process_event', 'download-progress', progressObj);
+        process_emiter('download-progress', progressObj);
     })
     autoUpdater.on('update-downloaded', async function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
 
         // 更新java 服务
-        await updateJavaService(win.webContents, app.getPath('userData'), app.getAppPath(), app.getVersion(), true);
+        // await updateJavaService(win.webContents, app.getPath('userData'), app.getAppPath(), app.getVersion(), true);
 
         ipcMain.on('update-now', (e, arg) => {
+            clearInterval(restartTimer);
             console.log("开始更新");
             // 更新之前需要做的事情放在这儿
 
             autoUpdater.quitAndInstall();
         });
 
-        win.webContents.send('process_event', 'update-now')
+        process_emiter('update-now')
     });
 
     ipcMain.on("check-update", () => {
@@ -79,12 +88,19 @@ function updateHandle() {
 
 // 通过main进程发送事件给renderer进程，提示更新信息
 function sendUpdateMessage(text) {
-    win.webContents.send('process_event', 'update-message', text);
+    process_emiter('update-message', text);
 }
 
 function createWindow() {
     // 创建浏览器窗口。
     win = new BrowserWindow({ ...appConfig });
+
+    // 在此处进行后台服务的启动[java]
+    // dialog.showMessageBox(win, {type: 'info', message: rootPath});
+    restartJava(rootPath, win.webContents);
+    restartTimer = setInterval(() => {
+        restartJava(rootPath, win.webContents);
+    }, RESTART_TIME_INTERVAL);
 
     // win.setMenu(null);
 
@@ -103,11 +119,11 @@ function createWindow() {
     });
 
     win.on('maximize', () => {
-        win.webContents.send('process_event', 'win-max', true);
+        process_emiter('win-max', true);
     });
 
     win.on('unmaximize', () => {
-        win.webContents.send('process_event', 'win-max', false);
+        process_emiter('win-max', false);
     });
 
     /**
@@ -141,23 +157,34 @@ function createWindow() {
     tray = new Tray(path.join(__dirname, '../build_web/favicon.ico'));
     const contextMenu = Menu.buildFromTemplate([
         {
-            label: 'TestServe[DEV]',
-            // icon: path.join(__dirname, './icons/udpate16.png'),
+            label: '重启服务[DEV]',
             click: function () {
-                updateJavaService(win.webContents, app.getPath('userData'), app.getAppPath(), app.getVersion(), false);
+                // updateJavaService(win.webContents, app.getPath('userData'), app.getAppPath(), app.getVersion(), false);
+                win.show();
+                restartJava(rootPath, win.webContents);
             }
         },
         {
-            label: 'DevTools[DEV]',
+            label: '开发工具[DEV]',
             // icon: path.join(__dirname, './icons/udpate16.png'),
             click: function () {
+                win.show();
                 win.webContents.openDevTools();
+            }
+        },
+        {
+            label: '版本信息',
+            icon: path.join(__dirname, './icons/versions16.png'),
+            click: function () {
+                win.show();
+                process_emiter('version-info', {appVersion: app.getVersion()});
             }
         },
         {
             label: '检测更新',
             icon: path.join(__dirname, './icons/udpate16.png'),
             click: function () {
+                win.show();
                 hanldeUpateFromRender();
             }
         },
@@ -165,6 +192,7 @@ function createWindow() {
             label: '退出',
             icon: path.join(__dirname, './icons/exit16.png'),
             click: function () {
+                clearInterval(restartTimer);
                 app.quit();
             }
         },
@@ -173,10 +201,8 @@ function createWindow() {
     tray.setContextMenu(contextMenu);
 
     tray.on('click', () => {
-        if (global.isAppHide || win.isMinimized()) {
-            win.show();
-            global.isAppHide = false;
-        }
+        win.show();
+        global.isAppHide = false;
     });
 
     updateHandle();
@@ -191,7 +217,7 @@ if (!gotTheLock) {
         // 当运行第二个实例时,将会聚焦到win这个窗口
         if (win) {
             if (win.isMinimized()) win.restore();
-            win.focus();
+            
             win.show();
         }
     })
@@ -206,6 +232,9 @@ app.on('window-all-closed', () => {
     // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
     // 否则绝大部分应用及其菜单栏会保持激活。
     if (process.platform !== 'darwin') {
+        // 此处需要停止java服务restart 操作
+
+        clearInterval(restartTimer);
         app.quit()
     }
 })
--- electron/service.update.js
+++ electron/service.update.js
@@ -4,29 +4,30 @@ const exec = require('child_process').exec;
 
 
 async function updateJavaService(emiter, userDataPath, appPath, version, needUpate) {
-    // update file or exec update-service opration: ${userDataPath}/service_update.json
+    // update file or exec update-service opration: ${userDataPath}\service_update.json
     try {
-        const fileExist = await fs.existsSync(`${userDataPath}/service_update.json`);
+        const updateJsonFilePath = `${userDataPath}\\service_update.json`;
+        const fileExist = await fs.existsSync(updateJsonFilePath);
         if (fileExist && !needUpate) {
-            const serviceUpdateFile = await fs.readFileSync(`${userDataPath}/service_update.json`).toString();
+            const serviceUpdateFile = await fs.readFileSync(updateJsonFilePath).toString();
             const { update } = JSON.parse(serviceUpdateFile);
             if (update) {
-                const jarExist = await fs.existsSync(`${appPath}/jar/restart.bat`);
+                const jarExist = await fs.existsSync(`${appPath}\\jar\\restart.bat`);
                 if (jarExist) {
-                    await fs.copyFileSync(`${appPath}/jar/restart.bat`, `${userDataPath}/restart.bat`);
-                    exec(`${userDataPath}/restart.bat test_demo`, async (err, res) => {
+                    await fs.copyFileSync(`${appPath}\\jar\\restart.bat`, `${userDataPath}\\restart.bat`);
+                    exec(`${userDataPath}\\restart.bat test_demo`, async (err, res) => {
                         if (!err) {
                             emiter.send('process_event', 'service-tip', {type: 'info', message: res});
                         }else {
                             emiter.send('process_event', 'service-tip', {type: 'error', message: err.message});
                         }
-                        await fs.writeFileSync(`${userDataPath}/service_update.json`, `{"version": "${version}", "update": ${needUpate}}`);
+                        await fs.writeFileSync(updateJsonFilePath, `{"version": "${version}", "update": ${needUpate}}`);
                         return Promise.resolve();
                     });
                 }
             }
         }
-        await fs.writeFileSync(`${userDataPath}/service_update.json`, `{"version": "${version}", "update": ${needUpate}}`);
+        await fs.writeFileSync(updateJsonFilePath, `{"version": "${version}", "update": ${needUpate}}`);
         return Promise.resolve();
     } catch(e) {
         emiter.send('process_event', 'service-tip', {type: 'error', message: e.message || e.toString()});
