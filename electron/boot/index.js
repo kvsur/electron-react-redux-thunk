6e54a78dbd55e940e671aaae4f8651abb6169bf5
znkf <znkf@myhexin.com>
2019-08-27 10:27:45 +0800

    add autoStartBoot util

---
+++ electron/boot/index.js
@@ -0,0 +1,47 @@
+// 引用winreg模块
+const WinReg = require('winreg');
+
+class Boot {
+    // 设置自动启动
+    static enableAutoStart({ name, file }, callback) {
+        var key = getKey();
+        key.set(name, WinReg.REG_SZ, file, callback || noop);
+    }
+
+    // 取消自动启动
+    static disableAutoStart({ name }, callback) {
+        var key = getKey();
+        key.remove(name, callback || noop);
+    }
+
+    // 获取是否自动启动
+    static getAutoStartValue({ name }, callback) {
+        var key = getKey();
+        key.get(name, function (error, result) {
+            if (result) {
+                callback(null, result.value);
+            } else {
+                callback(error);
+            }
+        })
+    }
+}
+
+var RUN_LOCATION = '\\Software\\Microsoft\\Windows\\CurrentVersion\\Run';
+
+// 获取注册表key
+function getKey() {
+    return new WinReg({
+        hive: WinReg.HKCU, // CurrentUser,
+        // hive: WinReg.HKLM, // 系统管理员
+        key: RUN_LOCATION,
+    })
+}
+
+// callback自定义方法，你可以在这里写代码
+function noop() {
+
+}
+
+// 导出
+module.exports = Boot;
